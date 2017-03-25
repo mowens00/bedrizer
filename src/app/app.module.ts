4786ed5a1317f1bb30c710d633c0d210e908afa2
Matt Owens <mowens@blacklbl.com>
2017-03-25 13:17:23 -0500

    firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCfYZgyB5O7KGZ5RPRzKo0iuF9gNHnoiWE",
+    authDomain: "bedrizer-9d01c.firebaseapp.com",
+    databaseURL: "https://bedrizer-9d01c.firebaseio.com",
+    storageBucket: "bedrizer-9d01c.appspot.com",
+    messagingSenderId: "633017329060"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
